350e4521ce070fd7098b93dbfcf29dbbfc079331
bagusyanuar <bagus.yanuar613@gmail.com>
2024-08-06 01:23:10 +0700

    a

--- app/rent/[id]/page.tsx
+++ app/rent/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { AxiosServerInstance } from '@/lib/axios';
 import RentDetailSourcePage from './source.page'
 import { getIronSessionData } from '@/lib/session'
-import { RentDetailModel } from '@/model/rent';
+import { RentDetailModel, RentCart, RentDriver } from '@/model/rent';
 import { notFound } from 'next/navigation';
 
 export default async function RentDetailPage({ params }: { params: { id: string } }) {
@@ -21,6 +21,50 @@ export default async function RentDetailPage({ params }: { params: { id: string 
         }
     })
     let data = serverResponse.data['data'];
+
+    let dataCarts = data['carts'] as Array<any>;
+    let dataRentDriver = data['rent_driver'] as Array<any>;
+
+    let arrCart: Array<RentCart> = [];
+    let arrRentDriver: Array<RentDriver> = [];
+
+    dataCarts.forEach((v, k) => {
+        let tmpCart: RentCart = {
+            ID: v['id'],
+            Price: v['price'],
+            Product: {
+                ID: v['product']['id'],
+                Name: v['product']['name'],
+                Image: `${ServerImage}${v['product']['image']}`
+            }
+        }
+        arrCart.push(tmpCart)
+    })
+
+    dataRentDriver.forEach((v, k) => {
+        let tmpRentDriver: RentDriver = {
+            ID: v['id'],
+            Price: v['price'],
+            Profile: {
+                ID: v['driver']['id'],
+                Image: `${ServerImage}${v['driver']['image']}`,
+                Name: v['driver']['name']
+            }
+        }
+        arrRentDriver.push(tmpRentDriver)
+    })
+
+    let dataRentDetail: RentDetailModel = {
+        ID: data['id'],
+        ReferenceNumber: data['reference_number'],
+        DateRent: data['date_rent'],
+        DateReturn: data['date_return'],
+        Status: data['status'],
+        Total: data['total'],
+        Carts: arrCart,
+        Drivers: arrRentDriver
+    }
+
     console.log(data);
     return <RentDetailSourcePage isAuth={isAuth} />
 }
