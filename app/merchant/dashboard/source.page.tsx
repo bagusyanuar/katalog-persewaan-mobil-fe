2d201f814000ed291836ea77222d5b0dd8113692
bagusyanuar <bagus.yanuar613@gmail.com>
2024-08-08 18:05:44 +0700

    a

--- app/merchant/dashboard/source.page.tsx
+++ app/merchant/dashboard/source.page.tsx
@@ -1,17 +1,23 @@
 'use client'
 
 import React from 'react'
-import Sidebar from './sidebar'
-import SidebarItem from './sidebar.item'
+import Sidebar from '@/components/navigation/sidebar/sidebar'
+import SidebarItem from '@/components/navigation/sidebar/sidebar.item'
 import { usePathname } from 'next/navigation'
+import NavbarMerchant from '@/components/navigation/navbar.merchant'
+import BreadcrumbWrapper from '@/components/breadcrumb/wrapper'
+import styled from 'styled-components'
+import Image from 'next/image'
+import LogoImage from '@/public/assets/images/logo.png'
+import { ColorPallete } from '@/components/color'
 
 
 const MerchantDashboarSourcePage: React.FC = () => {
-  
+
   const pathName = usePathname()
 
   return (
-    <main>
+    <Wrapper>
       <Sidebar>
         <SidebarItem
           to='/merchant/dashboard'
@@ -38,8 +44,64 @@ const MerchantDashboarSourcePage: React.FC = () => {
           active={pathName.startsWith('/merchant/order')}
         />
       </Sidebar>
-    </main>
+      <ContentWrapper>
+        <NavbarMerchant />
+        <ContentContainer>
+          <HeaderContainer>
+            <HeaderTitle>Halaman Driver</HeaderTitle>
+            <BreadcrumbWrapper>
+              <a href='#' className='active'>Dashboard</a>
+            </BreadcrumbWrapper>
+          </HeaderContainer>
+          <div className='w-full h-[400px] flex items-center justify-center flex-col gap-3'>
+            <Image src={LogoImage} alt='logo-image' height={150} width={150} priority />
+            <DashboardText>Selamat Datang Di Dashboard Car Rental</DashboardText>
+          </div>
+        </ContentContainer>
+      </ContentWrapper>
+    </Wrapper>
   )
 }
 
 export default MerchantDashboarSourcePage
+
+const DashboardText = styled.p`
+  font-size: 1em;
+  font-weight: bold;
+  color: ${ColorPallete.dark};
+  margin-bottom: 0;
+  width: 100%;
+  text-align: center;
+`
+const Wrapper = styled.div`
+    width: 100%;
+    display: flex;
+`
+
+const ContentWrapper = styled.div`
+    padding-left: 260px;
+    height: 100vh;
+    width: 100%;
+`
+
+const ContentContainer = styled.div`
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+`
+
+const HeaderContainer = styled.div`
+    margin-top: 1rem;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 3rem;
+`
+
+const HeaderTitle = styled.p`
+    margin-bottom: 0;
+    font-size: 1em;
+    color: ${ColorPallete.dark};
+    font-weight: bold;
+`
